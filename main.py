4ae988f1b378a30188e9ba25e423a899c215601c
Marco-Senpai <mcneilmarcus92@gmail.com>
2019-04-24 16:30:10 -0500

    final product

--- main.py
+++ main.py
@@ -1,66 +1,39 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import cgi
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True 
 
-
-page_header = """
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-5" />
-        <meta name="viewport" content="width=device-width, initial scale=1.0 />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>Validation</title>
-        <link rel="stylesheet" href="/static/app.css" />
-    </head>
-    <body>
-"""
-
-welcomeMessaging = """
-<h1>Wlcome to the page</h1>
-<a href="/register">Register</a> """
-
-page_footer = """
-    </body>
-</html>
-"""
-
-register_form = """
-<form action="/register" id="form" method="POST">
-    <h1>Register Please</h1>
-    <label for="username">Username</label>
-    <input type="text" name="username" id="username" value="{0}" />
-    <p class="error">{1}</p>
-    <label for="password">Password</lable>
-    <input type="password" name="password" id="password" value="{2}" />
-    <p class="error">{3}</p>
-    <label for="password">Password Confirmation</label>
-    <input type="password" name="password2" id="password2" value="{4}" />
-    <p class="error">{5}</p>
-    <button type="submit">Register</button>
-</form>
-"""
-
 @app.route("/register", methods=['POST'])
 def register():
     username = cgi.escape(request.form['username'])
     password = cgi.escape(request.form['password'])
     password2 = cgi.escape(request.form['password2'])
+    email = cgi.escape(request.form['email'])
 
     usernameError =""
     passwordError =""
     password2Error =""
+    emailError =""
 
     if not username:
         print("no username")
         usernameError = "Username is required to continue"
+    elif len(username) < 3:
+        usernameError = "Username must be at least 3 characters in length"
+    elif ' ' in username:
+        usernameError = "Username can not contain spaces"
     if not password:
         passwordError = "Passord is required to continue"
-    elif len(password) < 5:
-        passwordError = "Password must be at least 5 characters in length"
+    elif len(password) < 3:
+        passwordError = "Password must be at least 3 characters in length"
+    elif ' ' in password:
+        passwordError = "Password can not contain spaces"
+    
+    if '@' not in email or '.' not in email:
+        emailError = "Must include a valid email"
+    
     else:
         hasNumber = False
         for char in password:
@@ -71,22 +44,24 @@ def register():
     if password != password2:
         password2Error = "Password confirmation must match password"
 
-    if usernameError or passwordError or password2Error:
+    if usernameError or passwordError or password2Error or emailError:
         print("There is an error!")
-        content = page_header + register_form.format(username, usernameError, password, passwordError, password2, password2Error) + page_footer
-        return content
+        return render_template('welcome.html',username=username, usernameError=usernameError, password=password, passwordError=passwordError, password2=password2, password2Error=password2Error, emailError=emailError)
+    
 
     return "Thank you for registering, " + username
 
+    
+
 @app.route("/")
 def index():
-    content = page_header + welcomeMessaging + page_footer
-    return content
+
+    return render_template('base.html')
+
 
 @app.route("/register", methods=['GET'])
 def register_page():
-    content = page_header + register_form.format("", "", "", "", "", "") + page_footer
-    return content
+    return render_template('welcome.html', username='', usernameError='', password='', passwordError='', password2='', password2Error='', email='', emailError='')
 
 app.run()
 
